8df5a72c7e8cc60d1d7e91c3c28a5e5e5449bbfe
Avinash Kumar <165828196+avinasha6@users.noreply.github.com>
2025-05-10 20:44:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+ const board = document.getElementById('board');
+    for (let i = 0; i < 800; i++) {
+      const box = document.createElement('div');
+      box.classList.add('box');
+      board.appendChild(box);
+    }
